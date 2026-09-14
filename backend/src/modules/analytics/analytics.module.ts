2d748ecdeb55566bb6872c0a87cd783c29562472
sha <buccusshad@gmail.com>
2026-09-14 02:57:06 +0200

    fix: Register TokenTracerController and TokenTracerService in AnalyticsModule
    - Add TokenTracerController to AnalyticsModule controllers
    - Add TokenTracerService to AnalyticsModule providers
    - Enable token tracing endpoints: /tokens/trace/:address/:chain

--- backend/src/modules/analytics/analytics.module.ts
+++ backend/src/modules/analytics/analytics.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
+import { TokenTracerService } from './token-tracer.service';
+import { TokenTracerController } from './token-tracer.controller';
 import { Transaction } from '../transactions/entities/transaction.entity';
 import { Position } from '../positions/entities/position.entity';
 import { Wallet } from '../wallets/entities/wallet.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction, Position, Wallet])],
-  controllers: [AnalyticsController],
-  providers: [AnalyticsService],
+  controllers: [AnalyticsController, TokenTracerController],
+  providers: [AnalyticsService, TokenTracerService],
 })
 export class AnalyticsModule {}
